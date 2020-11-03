c9041d5110d2dc4f9cd503c7831c1d4640625080
Benjamin Williams <DeanPBR@gmail.com>
2020-11-03 11:57:29 -0500

    Created header from JavaScript and update recipe as well

---
+++ js/sampleTaco.js
@@ -0,0 +1,36 @@
+const sampleTaco = {
+    mixin: {
+        slug: "tomatoes_traditional_us",
+        url: "https://raw.github.com/sinker/tacofancy/master/mixins/tomatoes_traditional_us.md",
+        name: "Tomatoes (Traditional; US)",
+        recipe: "Tomatoes (Traditional; US) ====================== Adding a bit of coolness and moisture, tomatoes are a staple ingredient of the American taco. The better the tomato the more flavor they will impart, but even a sub-sandard grocery store tomato has a place in a traditional taco. * Tomatoes (diced) * Cherry Tomatoes (quartered) * Grape Tomatoes (sliced) tags: vegetarian "
+    },
+    base_layer: {
+        slug: "ground_beef_traditional_us",
+        url: "https://raw.github.com/sinker/tacofancy/master/base_layers/ground_beef_traditional_us.md",
+        name: "Ground Beef (Traditional; US)",
+        recipe: "Ground Beef (Traditional; US) ================== The tranditional base for an American taco is ground beef, browsed in a skillet and seasoned. * 1lb Ground Beef 1. Heat a skillet on the stovetop. 2. Brown the ground beef for 8-10 minutes, or until it is thoroughly cooked, stirring frequently to avoid sticking or burning. (If you're not using a non-stick pan, which is preferred, use 1T oil to keep things loose.) 3. Stir in seasonings."
+    },
+    seasoning: {
+        slug: "basic_dry_rub",
+        url: "https://raw.github.com/sinker/tacofancy/master/seasonings/basic_dry_rub.md",
+        name: "Basic Dry Rub",
+        recipe: "Basic Dry Rub ================= This is a great, versatile recipe to use as a taco seasoning or as a dry rub for the protein of your choice. Equally at home in the skillet, oven, stovetop, or smoker. * 2 Tbs Cumin * 2 Tbs Chili Powder * 1 Tbs Salt * 1 Tbs Garlic Powder * 2 tsp Black Pepper * 1 tsp Chipotle Powder Stir to combine and apply liberally. tags: vegetarian, vegan "
+    },
+    condiment: {
+        slug: "guacamole",
+        url: "https://raw.github.com/sinker/tacofancy/master/condiments/guacamole.md",
+        name: "Guacamole",
+        recipe: "Guacamole ========= This is pretty canonical stuff right here. * 1-2 ripe Hass avocados * half a lime * a clove or two of chopped garlic * some chopped white or yellow onion * salt * pepper * dry cayenne or red chile pepper powder * a jalapeño, if that's your thing * a small chopped tomato * a handful of chopped cilantro Look, here's how this works: First, you squeeze the lime juice into your nice little ceramic bowl. Then you add the garlic, onion, spices, peppers if you're heating it up, and then **walk away**. Seriously, walk away for a minute. Crack open a nice Negra Modelo and use a wedge of the other half of the lime in it. Back in your bowl, let the strong stuff marinate in that lime juice for a few minutes. It's cooking. OK, now that you're back, mix in the rest of the non-avocado ingredients. If you have a pestle, now's it's time to shine. If not, use a small wooden spoon, or failing all else, a fork, you miserable lout. Mash up everything but the avocado. Get it pasty if you can. Squish it all up. Now you can add your avocado or two, dude. I usually go with about 1.5 avos to condiment tacos or taco-like structures for four humans of varying sizes. Just in case no one ever taught you how to deal with an avocado correctly, here's a quick lesson: * Cut it in half, longways. * Smack the pit with the sharp edge of your knife and give it a sensible 90-degree twist. * Voila, the avocado pit is on your knife. * Dispose of it, unless you're not going to finish all that guac now, in which case, hang onto it and put it in the bowl with the guac before you put it in the fridge. * Now that your knife is free, use it to carve long slices into the flesh of your avocado, one half at a time. Cross-hatch that sucker a bit. Don't cut through the flesh. * Finally, get your avocado half above your nice bowl full of deliciousness, and use the skin to gently push your nice slices into the spicy soup below. Turn it inside-out, if you must, but know that you can minimize the amount of avocado that gets all over your hands if you do this just right. To finish up your guacamole, mash your avocado up a bit and gently mix it into the magical citric-spice-tomato concoction. We're done here. _Thanks to [The Awl](http://www.theawl.com/2010/02/half-baked-guacamole-by-the-ceviche-method) for limited inspiration regarding the limey and drinky parts._ tags: vegetarian "
+    },
+    shell: {
+        slug: "bad_ass_tortillas",
+        url: "https://raw.github.com/sinker/tacofancy/master/shells/homemade_tortillas.md",
+        name: "bad-ass tortillas",
+        recipe: "bad-ass tortillas ===================== If you are making tacos, don't settle for corporate store bought tortillas. Make your own like a real person! First get a cast iron pan: ![](./pan.jpg) and then one of these bad-ass tortilla presses: ![](./tortillador.jpg) Buy your lard from a place like this: ![](./store.jpg) * 2 cups all purpose flour * 1/4 cup lard (cut into lil' pieces) * 1 teaspoon kosher salt * 2/3 tablespoon oil * 1/2 cup water (luke warm) Mix all ingredients together except oil and water. Drizzle oil over mixture and mix with hands. Add water and mix and knead again until doughy. Let chill for about an hour in plastic wrappers. Heat large cast iron skillet (or something more authentic if you've got it) over medium heat. Cut dough into about 12 pieces that are round. Use a proper tortilla press (or something more authentic if you've got it) to make 'em flat and then put on the skillet. Wait until the transparent parts turn opaque and flip em. Put cooked tortillas in a *dirty* cloth napkin to keep 'em warm. End recipe. Paz, amor, y dinero. "
+    }
+}
+
+export {
+    sampleTaco
+}
